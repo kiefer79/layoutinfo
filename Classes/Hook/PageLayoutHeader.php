24623784c9e5db35253cc8515a4f17cbd9c0c247
Andreas Kiefer <andreas.kiefer@pluswerk.ag>
2018-11-16 12:21:08 +0100

    [TASK] render be layout info in page module toolbar

--- Classes/Hook/PageLayoutHeader.php
+++ Classes/Hook/PageLayoutHeader.php
@@ -2,42 +2,55 @@
 
 namespace Kiefer\LayoutInfo\Hook;
 
+use TYPO3\CMS\Backend\Template\Components\ButtonBar;
 use TYPO3\CMS\Backend\View\BackendLayoutView;
-use TYPO3\CMS\Core\Messaging\FlashMessage;
-use TYPO3\CMS\Core\Messaging\FlashMessageService;
+use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3\CMS\Backend\View\BackendLayout\BackendLayout;
+use TYPO3\CMS\Backend\Controller\PageLayoutController;
 
 class PageLayoutHeader
 {
 
-    public function render($parameters, $parentObject)
+    /**
+     * @var int $id
+     */
+    protected $id;
+
+
+    public function __construct()
+    {
+        $this->id = (int)$GLOBALS['_GET']['id'];
+    }
+
+    /**
+     * @param $parameters
+     * @param PageLayoutController $pageLayoutController
+     */
+    public function render($parameters, $pageLayoutController)
     {
         $view = GeneralUtility::makeInstance(BackendLayoutView::class);
-        $layoutIdentifier = $view->getSelectedCombinedIdentifier($parentObject->id);
+        $layoutIdentifier = $view->getSelectedCombinedIdentifier($this->id);
         $dataProviderCollection = $view->getDataProviderCollection();
-        $backendLayout = $dataProviderCollection->getBackendLayout($layoutIdentifier, $parentObject->id);
+        $backendLayout = $dataProviderCollection->getBackendLayout($layoutIdentifier, $this->id);
 
         if ($backendLayout instanceof BackendLayout) {
             $backendLayoutTitle = $this->getLanguageService()->sL($backendLayout->getTitle());
-            $messageText = 'Backend layout in use: ' . $backendLayoutTitle;
         } else {
-            $messageText = 'No backend layout selected';
+            $backendLayoutTitle = 'None';
         }
 
-        $message = GeneralUtility::makeInstance(FlashMessage::class,
-            $messageText,
-            '',
-            FlashMessage::INFO,
-            false
-        );
+        $content =
+            $pageLayoutController->iconFactory->getIcon('mimetypes-x-backend_layout', Icon::SIZE_SMALL)->render() . '&nbsp;' .
+            $this->getLanguageService()->sL('LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:backend_layout') .
+            ': ' . $backendLayoutTitle;
 
-        $objectManager = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
-        $flashMessageService = $objectManager->get(FlashMessageService::class);
-        $messageQueue = $flashMessageService->getMessageQueueByIdentifier();
-        $messageQueue->addMessage($message);
-
+        /** @var ButtonBar $buttonBar */
+        $buttonBar = $pageLayoutController->moduleTemplate->getDocHeaderComponent()->getButtonBar();
+        $layoutInfoButton = $buttonBar
+            ->makeFullyRenderedButton()
+            ->setHtmlSource($content);
+        $buttonBar->addButton($layoutInfoButton, ButtonBar::BUTTON_POSITION_RIGHT);
     }
 
     /**
